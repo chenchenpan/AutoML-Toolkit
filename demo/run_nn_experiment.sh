fd89d2c8c11efb460082643626c31ba31b92ea61
Ubuntu <chenchenpan@chenchenpan-dsvm-linux-nd.qbz4zmhoeqqezc35upkxmo0owf.bx.internal.cloudapp.net>
2024-03-28 22:57:39 +0000

    modify demo code and vm_setup.sh

--- demo/run_nn_experiment.sh
+++ demo/run_nn_experiment.sh
@@ -1,14 +1,17 @@
-export DIR=$HOME/projects/Amplify-AutoML-Toolkit
+export H=/home/chenchenpan/Projects
+export DIR=$H/AutoML-Toolkit
 export DATA_DIR=$DIR/demo/data/raw_data
 export GLOVE_DIR=$DIR/resource/glove
 export ENCODED_DATA_DIR=$DIR/demo/data/encoded_data_comb_glove
 
 python $DIR/encode_data.py \
        --output_dir $ENCODED_DATA_DIR \
-       --metadata_file $DATA_DIR/metadata_comb.json \
-       --train_file $DATA_DIR/comb_train.tsv \
-       --dev_file $DATA_DIR/comb_dev.tsv \
-       --test_file $DATA_DIR/comb_test.tsv \
+       --data_dir $DATA_DIR \
+       --metadata_dir $DATA_DIR \
+       --metadata_file metadata_comb.json \
+       --train_file comb_train.tsv \
+       --dev_file comb_dev.tsv \
+       --test_file comb_test.tsv \
        --use_text_features True \
        --encode_text_with glove\
        --glove_file $GLOVE_DIR/glove.6B.50d.txt \
--- vm_setup.sh
+++ vm_setup.sh
@@ -1,10 +1,10 @@
-cd ~/projects/
+cd ~/Projects/
 pip install virtualenv
 mkdir envs && cd envs
 virtualenv lab
 
 source lab/bin/activate
-cd ~/projects/Amplify-AutoML-Toolkit/
+cd ~/Projects/AutoML-Toolkit/
 pip install -r requirements.txt 
 
 deactivate
